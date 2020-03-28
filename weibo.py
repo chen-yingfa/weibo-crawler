571ab0dda863956fa5e33d9e7d582507d82a6ca9
Blazing-Bonfire <chenyingfa1999@qq.com>
2020-03-28 23:42:20 +0800

    add comment and more example crawled data

--- weibo.py
+++ weibo.py
@@ -11,6 +11,7 @@ from scrapy.crawler import CrawlerProcess
 
 SLEEP_TIME = 2
 
+# 每个微博条是一个类
 class Post:
     mid = 0
     author = ""
@@ -33,28 +34,26 @@ class Post:
         self.likes = likes
         self.content = content
     
+# 整个爬虫
 class WeiboSpider(scrapy.Spider):
-    name = 'weibo'
-    cookies = """
+    name = 'weibo' # 爬虫名称
+    cookies = """  由构造函数赋值
     """
     allowed_domains = ['weibo.com']
 
-    # 搜索参数
+    # 搜索参数（由构造函数赋值）
     keywords = [] # 关键词
-    start_date = date(2020, 1, 1)
+    start_date = date(2020, 1, 1) 
     end_date = date(2020, 1, 1)
     list_prov = [] # 省份
     
-    
-    locations = []
-    # ex_url = "https://s.weibo.com/weibo/疫情&region=custom:11:8&typeall=1&suball=1&timescope=custom:2020-02-01-0:2020-02-01-1&Refer=g&page=11"
-    baseUrl = "https://s.weibo.com/weibo/{}&region=custom:{}:{}&typeall=1&suball=1&timescope=custom:{}:{}&Refer=g&page={}"
-    start_urls = []
-    posts = {}
+    locations = [] # 所有（市级）地点
+    baseUrl = "https://s.weibo.com/weibo/{}&region=custom:{}:{}&typeall=1&suball=1&timescope=custom:{}:{}&Refer=g&page={}" 
+    posts = {} # 爬取的所有微博条都存到posts，是一个dict of list of Post。dict的key是省份名称。
 
-    data_dir = "./data/"
-    num_unsaved_post = 0
-    num_post_total = 0
+    data_dir = "./data/" # 数据库文件的文件夹
+    num_unsaved_post = 0 # 记录仍未保存多少条
+    num_post_total = 0   # 记录总共爬取多少条
 
     ###### utility ########
     def add_post(self, post):
@@ -62,9 +61,15 @@ class WeiboSpider(scrapy.Spider):
         # print("< added post >")
 
     def strToDate(self, s):
+        """
+        str转换成date
+        """
         return date(int(s[:4]), int(s[5:7]), int(s[8:10]))
 
     def list_str_to_Post(self, l):
+        """
+        将list of str转换成Post，其中str顺序：mid, author, upload_date, prov, city, reposts, comments, likes, content
+        """
         mid = int(l[0])
         upload_date = self.strToDate(l[2])
         prov = l[3]
@@ -77,13 +82,22 @@ class WeiboSpider(scrapy.Spider):
         return Post(mid, l[1], upload_date, prov, city, reposts, comments, likes, content)
 
     def get_url(self, keyword, prov_id, city_id, start_time, end_time, page = 1):
+        """
+        返回对应着参数的url
+        """
         return self.baseUrl.format(keyword, str(prov_id), str(city_id), start_time, end_time, str(page))
 
     def get_db_name(self, date, prov):
+        """
+        返回对应日期和省份的数据库文件夹名称
+        """
         prov_id = self.get_prov_id(prov)
         return self.data_dir + str(date) + "_" + str(prov_id) + ".db"
 
     def get_prov_id(self, prov):
+        """
+        返回省份的编号
+        """
         for location in self.locations:
             # print(location[1])
             if prov == location[1]:
@@ -93,6 +107,9 @@ class WeiboSpider(scrapy.Spider):
     ###### utility end ####
 
     def __init__(self, keywords, list_prov, start_date, end_date, cookies):
+        """
+        获取搜索参数
+        """
         self.keywords = keywords
         self.list_prov = list_prov
         self.start_date = start_date
@@ -100,6 +117,9 @@ class WeiboSpider(scrapy.Spider):
         self.cookies = cookies
 
     def create_db(self, date, prov):
+        """
+        若没有db文件（数据库）对应此日期和省份，创建对应的db文件
+        """
         prov_id = self.get_prov_id(prov)
         self.db_name = self.data_dir + str(date) + "_" + str(prov_id) + ".db"
 
@@ -123,6 +143,9 @@ class WeiboSpider(scrapy.Spider):
         conn.close()
 
     def load_locations(self):
+        """
+        载入所有市级地点
+        """
         self.locations = []
         with open('locations.csv', mode = 'r', encoding = 'utf-8') as locFile:
             r = csv.reader(locFile)
@@ -133,6 +156,9 @@ class WeiboSpider(scrapy.Spider):
         print('< loaded locations >')
 
     def load_posts(self):
+        """
+        从数据库载入微博
+        """
         conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
         
@@ -147,6 +173,9 @@ class WeiboSpider(scrapy.Spider):
         conn.close()
 
     def save_posts(self, date, prov):
+        """
+        保存微博条
+        """
         db_name = self.get_db_name(date, prov)
         conn = sqlite3.connect(db_name)
         c = conn.cursor()
@@ -155,9 +184,9 @@ class WeiboSpider(scrapy.Spider):
         # cmd = "DELETE FROM posts"
         # c.execute(cmd)
 
-        cnt = 0
+        cnt = 0 # 用于记录总共保存多少条微博
         for p in self.posts[prov][:]:
-            if p.prov != prov:
+            if p.prov != prov: 
                 continue
             c.execute("INSERT INTO posts values(?, ?, ?, ?, ?, ?, ?, ?, ?)", (
                     p.mid, 
@@ -170,7 +199,7 @@ class WeiboSpider(scrapy.Spider):
                     p.likes,
                     p.content)
                 )
-            self.posts[prov].remove(p)
+            self.posts[prov].remove(p) # 去掉已经保存的微博
             cnt += 1
         self.num_unsaved_post -= cnt
         conn.commit()
@@ -181,27 +210,32 @@ class WeiboSpider(scrapy.Spider):
         self.save_posts(date, prov)
 
     def start_requests(self):
+        """
+        根据输入搜索参数，生成对应的Request并且发送请求。
+        """
         self.load_locations()
 
+        # 创建posts和数据库文件
         for prov in self.list_prov:
             self.create_db(self.start_date, prov) # create a db file for each province
             self.posts[prov] = []   # init dict of list of posts
 
-        # add cookies for logging in
+        # 将cookies格式化
         self.cookies = {i.split("=")[0]:i.split("=")[1] for i in self.cookies.split("; ")}
 
-        # set time range of results
-        time_range = 2
+        # 设置一个时间范围防止搜索结果多于50页（1000条），因为微博不会返回多于50页的结果，所以会某些微博条
+        time_range = 2 # 默认2小时
 
-        # gen urls
+        # 生成url
         day = self.start_date
-        while day != self.end_date:
-            for location in self.locations:
+        while day != self.end_date: # 遍历天
+            for location in self.locations: # 遍历（市级）地点
                 prov_id, prov, city_id, city = location[0], location[1], location[2], location[3]
-                if not prov in self.list_prov:
+
+                if not prov in self.list_prov: # 不是目标省份的地点则跳过
                     continue
-                for keyword in self.keywords: 
-                    for i in range(0, 23, time_range):
+                for keyword in self.keywords: # 遍历关键词
+                    for i in range(0, 23, time_range): # 遍历一天内的每个时间段（默认两个小时）
                         start_time = str(day) + "-" + str(i)
                         end_time = str(day) + "-" + str(min(23, i + time_range))
 
@@ -231,6 +265,7 @@ class WeiboSpider(scrapy.Spider):
                         )
                         time.sleep(1)
 
+                    # 最后23:00到下一天的00:00的数据
                     start_time = str(day) + "-23"
                     end_time = str(day + datetime.timedelta(days = 1)) + "-0"
                     print("< yielding {}, from {} to {}, keyword: '{}' >".format(location, start_time, end_time, keyword))
@@ -257,10 +292,15 @@ class WeiboSpider(scrapy.Spider):
                         }
                     )
                     time.sleep(1)
-                self.save(day, prov)
-            day += datetime.timedelta(days = 1)
+                self.save(day, prov) # 保存
+            day += datetime.timedelta(days = 1) # 下一天
 
     def parse(self, response):
+        """
+        对于html进行parsing
+        """
+
+        # meta数据
         meta = response.meta
         url = meta["url"]
         page_num = meta["page_num"]
@@ -277,16 +317,16 @@ class WeiboSpider(scrapy.Spider):
 
         soup = BeautifulSoup(response.body, "html.parser")
         
-        # check if this page exist
+        # 检查是否到达尾页
         errorTag = soup("div", {"class": "m-error"})
         if len(errorTag) != 0:
-            # page does not exist
+            # 页不存在
             print("< page not found >")
             return
 
         # loop and parse each card
         list_card_wrap = soup("div", {"mid": True})
-        cnt = 0
+        cnt = 0 # 记录多少card
         for card_wrap in list_card_wrap:
             mid = 0
             author = ""
@@ -297,7 +337,7 @@ class WeiboSpider(scrapy.Spider):
             upload_date = date(1, 1, 1)
             # tags = []
 
-            # get mid and check if processed
+            # get mid (unique id of each weibo) and check if processed
             mid = int(card_wrap["mid"].strip())
             if mid in self.posts:
                 continue
@@ -325,7 +365,7 @@ class WeiboSpider(scrapy.Spider):
             fromTag = content_tag.find("p", {"class": "from"}, recursive = False)
             fromTxt = fromTag.get_text().strip()
 
-            #set values
+            # 给post的参数赋值
             author = nameTag.string.strip()
             content = txt_tag.get_text().strip()
             if content[-5:] == "收起全文d":
@@ -359,6 +399,7 @@ class WeiboSpider(scrapy.Spider):
 
             cnt += 1
         
+        # 记录总共爬取多少
         self.num_unsaved_post += cnt
         self.num_post_total += cnt
         print("< done parsing posts: {}, total: {}, unsaved: {} >".format(cnt, self.num_post_total, self.num_unsaved_post))
@@ -384,6 +425,7 @@ class WeiboSpider(scrapy.Spider):
             }
         )
 
+# 主函数
 if __name__ == '__main__':
     process = CrawlerProcess(settings={
         'FEED_FORMAT': 'json',
